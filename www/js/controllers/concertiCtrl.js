2845277a486c81144cd80835fc6f7d999d648fd1
pirolab <diegovalobra@gmail.com>
2016-03-16 12:31:35 +0100

    Allineati evento <-> concerto

--- www/js/controllers/concertiCtrl.js
+++ www/js/controllers/concertiCtrl.js
@@ -1,16 +1,16 @@
 controllersmodule.controller('ConcertiCtrl', function($scope, User) {
 	$scope.concerti = [
-    { title: 'Evento 1', id: 1 },
-    { title: 'Evento 2', id: 2 },
-    { title: 'Evento 3', id: 3 },
-    { title: 'Evento 4', id: 4 },
-    { title: 'Evento 5', id: 5 },
-    { title: 'Evento 6', id: 6 },
-    { title: 'Evento 7', id: 7 },
-    { title: 'Evento 8', id: 8 },
-    { title: 'Evento 9', id: 9 },
-    { title: 'Evento 10', id: 10 },
-    { title: 'Evento 11', id: 11 },
-    { title: 'Evento 12', id: 12 }
+    { title: 'Concerto 1', id: 1 },
+    { title: 'Concerto 2', id: 2 },
+    { title: 'Concerto 3', id: 3 },
+    { title: 'Concerto 4', id: 4 },
+    { title: 'Concerto 5', id: 5 },
+    { title: 'Concerto 6', id: 6 },
+    { title: 'Concerto 7', id: 7 },
+    { title: 'Concerto 8', id: 8 },
+    { title: 'Concerto 9', id: 9 },
+    { title: 'Concerto 10', id: 10 },
+    { title: 'Concerto 11', id: 11 },
+    { title: 'Concerto 12', id: 12 }
   ];
 });
