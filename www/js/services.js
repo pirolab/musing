0a006d132c7228a20a4e32e2fea8d3ed3dc52e6d
pirolab <diegovalobra@gmail.com>
2016-03-17 16:04:26 +0100

    fixato e testato json su API login

--- www/js/services.js
+++ www/js/services.js
@@ -14,46 +14,56 @@ angular.module('starter.services', [])
 	}
 	
 	//Let's test a post http call
-//	var posttest = function(){
-//		var data = {
-//			name: 'prova',
-//			pass: 'test'
-//		};
-//		$http.post('http://www.musingshow.com/it/api/rest/test',data).then(function(resp) {
-//			console.log('Success', resp);
-//			// For JSON responses, resp.data contains the result
-//		  }, function(err) {
-//			console.error('ERR', err);
-//			// err.status will contain the status code
-//		});
-//	}
-	/* Altro Approccio usando Object in POST e sembra funzionare */
 	var posttest = function(){
-		data_obj = { email: 'diegovalobra@gmail.com',password:'bella_zio' };        
-		Object.toparams = function ObjecttoParams(obj) {
-		  var p = [];
-		  for (var key in obj) {
-			p.push(key + '=' + encodeURIComponent(obj[key]));
-		  }
-		  return p.join('&');
+		var user = {
+			name: 'pirolab',
+			pass: 'samah_2012'
 		};
-		
-		var req = {
+		$http({
 			method: 'POST',
-			url: "http://www.musingshow.com/it/api/rest/test",
-			data: Object.toparams(data_obj),
+			url: 'http://www.musingshow.com/it/api/rest/get/userInfo/json?name='+user.name+'&pass='+user.pass,
 			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-		}
-		$http(req).
-		success(function(data, status, headers, config) 
-		{
-			console.log('Success, Dati mandati: '+ data +' staus: '+status)
-		}).
-		error(function(data, status, headers, config) 
-		{
-			console.log('faild '+ status)
+		}).then(function(resp) {
+			console.log('Success', resp.data.account);
+			var content ='';
+				content += resp.data.account.name+'<br />';
+				content += resp.data.account.isPasswordOk+'<br />';
+				content += resp.data.account.uid+'<br />';
+				content += resp.data.account.email+'<br />';
+				content += '<img src="'+resp.data.account.image+'" /><br />';
+			document.querySelector('.data').innerHTML = content;
+		  }, function(err) {
+			console.error('ERR', err);
+			// err.status will contain the status code
 		});
 	}
+	/* Altro Approccio usando Object in POST e sembra funzionare */
+//	var posttest = function(){
+//		data_obj = { email: 'diegovalobra@gmail.com',password:'bella_zio' };        
+//		Object.toparams = function ObjecttoParams(obj) {
+//		  var p = [];
+//		  for (var key in obj) {
+//			p.push(key + '=' + encodeURIComponent(obj[key]));
+//		  }
+//		  return p.join('&');
+//		};
+//		
+//		var req = {
+//			method: 'POST',
+//			url: "http://www.musingshow.com/it/api/rest/test",
+//			data: Object.toparams(data_obj),
+//			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+//		}
+//		$http(req).
+//		success(function(data, status, headers, config) 
+//		{
+//			console.log('Success, Dati mandati: '+ data +' staus: '+status)
+//		}).
+//		error(function(data, status, headers, config) 
+//		{
+//			console.log('faild '+ status)
+//		});
+//	}
 
 
 	var usertype = 'follower';
