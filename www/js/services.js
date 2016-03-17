fd146fc8e15f83742a4b9202eedc6fee1b961376
pirolab <diegovalobra@gmail.com>
2016-03-17 12:08:02 +0100

    Sistemate le chiamate POST, ora dovrebbe funzionare

--- www/js/services.js
+++ www/js/services.js
@@ -14,19 +14,47 @@ angular.module('starter.services', [])
 	}
 	
 	//Let's test a post http call
+//	var posttest = function(){
+//		var data = {
+//			name: 'prova',
+//			pass: 'test'
+//		};
+//		$http.post('http://www.musingshow.com/it/api/rest/test',data).then(function(resp) {
+//			console.log('Success', resp);
+//			// For JSON responses, resp.data contains the result
+//		  }, function(err) {
+//			console.error('ERR', err);
+//			// err.status will contain the status code
+//		});
+//	}
+	/* Altro Approccio usando Object in POST e sembra funzionare */
 	var posttest = function(){
-		var data = {
-			name: 'prova',
-			pass: 'test'
+		data_obj = { email: 'diegovalobra@gmail.com',password:'bella_zio' };        
+		Object.toparams = function ObjecttoParams(obj) {
+		  var p = [];
+		  for (var key in obj) {
+			p.push(key + '=' + encodeURIComponent(obj[key]));
+		  }
+		  return p.join('&');
 		};
-		$http.post('http://www.musingshow.com/it/api/rest/test',data).then(function(resp) {
-			console.log('Success', resp);
-			// For JSON responses, resp.data contains the result
-		  }, function(err) {
-			console.error('ERR', err);
-			// err.status will contain the status code
+		
+		var req = {
+			method: 'POST',
+			url: "http://www.musingshow.com/it/api/rest/test",
+			data: Object.toparams(data_obj),
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		}
+		$http(req).
+		success(function(data, status, headers, config) 
+		{
+			console.log('Success, Dati mandati: '+ data +' staus: '+status)
+		}).
+		error(function(data, status, headers, config) 
+		{
+			console.log('faild '+ status)
 		});
 	}
+
 
 	var usertype = 'follower';
 	var user = 'testuser';
